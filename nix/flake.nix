0c1c043065815e5e98d9fc25ae63dc2ce0841386
Jinser Kafka <aimer@purejs.icu>
2024-10-25 11:20:07 +0800

    dev: add flake

---
+++ nix/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A startup basic project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+
+      perSystem = { pkgs, ... }: {
+        devshells.default = {
+          packages = with pkgs; [
+            nodejs
+
+            vscode-langservers-extracted
+            nodePackages.typescript-language-server
+
+            python3
+            gnumake
+            gnat
+          ];
+        };
+      };
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+    };
+}
